7b62600516ac92c1cbd6453a1b25ca58d5af05df
Jhonatan Grueso <jhonper86@gmail.com>
2022-06-26 03:08:39 -0500

    Ejercicio2 python tema 4 version1.2

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
-numero_inicial = 2
-numero_final = 8
+numero_inicial = int(input("Digite el primer número: "))
+numero_final = int(input("Digite el segundo número:"))
 lista = []
 
+#Se repite el while() mientras el segundo número sea menor o igual que el primer número
+while numero_final <= numero_inicial:
+    numero_final = int(input("El segundo número debe ser mayor que el primero. Digite un nuevo número:"))
+
+#Itera sobre el rango dado entre los números ingresado
 for numero in range(numero_inicial,numero_final):
     if numero % 2 == 1:
         lista.append(numero)
 
-print(lista)
+print(f"Los números impares entre {numero_inicial} y {numero_final} son: {lista}") #[3,5,7]
